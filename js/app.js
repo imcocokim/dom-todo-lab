c99648f605a65b54e49d2d76c5ef3ee78d4eff45
imcoco <imcoco@gmail.com>
2022-05-31 17:09:32 -0400

    set up event evt function

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 /* cache elements */
-const input = document.querySelector("#input")
+const inp = document.querySelector("#input")
 const btn = document.querySelector("#submit-button")
 const toDoList = document.querySelector("#todo-list")
 
 /* event listeners */
-btn.addEventListener("submit-button", onclick)
+btn.addEventListener("submit-button", evt)
 
+/* functions */
+function evt() {
+  const li = document.createElement("li")
+  const inp = document.querySelector("input")
+  console.log(li)
+}
